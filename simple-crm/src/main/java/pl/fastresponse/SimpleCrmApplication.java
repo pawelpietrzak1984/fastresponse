3b6f094ddd2834082a0c82df95534f6aaffe022d
pawelpietrzak1984 <ppietrzak@LL150117.kruk-inkaso.com.pl>
2015-04-16 10:30:10 +0200

    init

---
+++ simple-crm/src/main/java/pl/fastresponse/SimpleCrmApplication.java
@@ -0,0 +1,12 @@
+package pl.fastresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrmApplication.class, args);
+    }
+}
---
+++ simple-crm/src/test/java/pl/fastresponse/SimpleCrmApplicationTests.java
@@ -0,0 +1,18 @@
+package pl.fastresponse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SimpleCrmApplication.class)
+@WebAppConfiguration
+public class SimpleCrmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
