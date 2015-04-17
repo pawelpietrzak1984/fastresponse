4bf4e29592595beb855f2615d894c49bcf846bcf
pawelpietrzak1984 <ppietrzak@LL150117.kruk-inkaso.com.pl>
2015-04-17 14:46:35 +0200

    pmd

--- simple-crm/src/main/java/pl/fastresponse/SimpleCrmApplication.java
+++ simple-crm/src/main/java/pl/fastresponse/SimpleCrmApplication.java
@@ -2,7 +2,7 @@ package pl.fastresponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.aopalliance.aop.*;
 @SpringBootApplication
 public class SimpleCrmApplication {
 
