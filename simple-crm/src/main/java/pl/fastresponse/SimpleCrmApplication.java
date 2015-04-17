345602421dd7a3fcadf977321b5dde972e64c556
pawelpietrzak1984 <ppietrzak@LL150117.kruk-inkaso.com.pl>
2015-04-17 14:12:11 +0200

    a

--- simple-crm/src/main/java/pl/fastresponse/SimpleCrmApplication.java
+++ simple-crm/src/main/java/pl/fastresponse/SimpleCrmApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleCrmApplication {
 
+	private String unused;
+	
     public static void main(String[] args) {
+    	
+    	String unused2 = "as";
+    	
         SpringApplication.run(SimpleCrmApplication.class, args);
     }
 }
